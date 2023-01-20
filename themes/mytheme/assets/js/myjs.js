a7573716bfc8de73bdd308f9fec342ab0c0e7aa5
marinzelenika2603 <marin.zelenika2603@gmail.com>
2023-01-20 13:11:46 +0100

    displayed all the prices and product names

--- themes/mytheme/assets/js/myjs.js
+++ themes/mytheme/assets/js/myjs.js
@@ -12,19 +12,22 @@ fetch(
         .then((response) => response.json())
         .then((data) => {
           let productFamilyData = data.Products;
-          
           productFamilyData.forEach((product) => {
+            let productName = product.Name;
             product.PricingPlans.filter((plan) => plan.DisplayOrder > 0)
               .sort((a, b) => a.DisplayOrder - b.DisplayOrder)
               .forEach((plan) => {
                 plan.PaymentScheduleList.forEach((schedule) => {
-                    document.getElementById("product-names").innerHTML =
-                      data.Name;
-                  charges += schedule.SubscriptionPeriodCharge;
+                  charges += schedule.SubscriptionPeriodCharge + " ";
                 });
               });
-            document.getElementById("product-pricing-plans").innerHTML +=
-              charges;
+            document.getElementById(
+              "product-names"
+            ).innerHTML += `<h2>${productName}</h2>`;
+            document.getElementById(
+              "product-pricing-plans"
+            ).innerHTML += `<p>${charges}</p>`;
+            charges = "";
           });
         });
     });
